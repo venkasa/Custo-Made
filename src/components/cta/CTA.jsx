5c3ee6ea072e53cfd373462a9f1f20aa17e3c677
VenkateshKrishna <venkateshkrishna997@gmail.com>
2023-01-10 19:19:27 +0530

    fixes input names in yup validator and errors

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,15 +5,15 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = yup.object().shape({
-  Name: yup.string().required("name is required"),
-  CompanyName: yup.string().required("company name is required"),
-  Quantity: yup.number().integer().positive().required("quantity is required"),
-  ContactNumber: yup
+  name: yup.string().required("name is required"),
+  companyName: yup.string().required("company name is required"),
+  quantity: yup.number().integer().positive().required("quantity is required"),
+  contactNumber: yup
     .number()
     .integer()
     .positive()
     .required("phone number is required"),
-  Email: yup.string().email().required("enter Email"),
+  email: yup.string().email().required("enter Email"),
   city: yup.string().required("city is required"),
 });
 
@@ -24,49 +24,44 @@ const CTA = () => {
     formState: { errors, isValid, isDirty },
   } = useForm({
     resolver: yupResolver(schema),
-    mode: "onChange",
   });
+
+  const onSubmit = (data) => {
+    const body = `${data.name} from ${data.companyName} with contact number ${data.contactNumber} and email ${data.email} from city ${data.city} would like to contact you for ${data.quantity} items`;
+    window.location.href = `mailto:customademanufacturer@gmail.com?subject=Product Enquiry&body=${body}`;
+  };
+
   return (
     <>
       <div
         className="header section__padding products__main-div our__products"
         id="CTA"
       >
-        <div id="reach-us" className="header-content gradient__text title">
-          Reach Us.
-        </div>
+        <div className="header-content gradient__text title">Reach Us.</div>
         <div className="inputs">
-          <form
-            action="https://formspree.io/f/moqzanpz"
-            method="POST"
-            className="form"
-            onSubmit={handleSubmit((data) => {
-              const body = `${data.name} from ${data.companyName} with contact number ${data.contactNumber} and email ${data.email} from city ${data.city} would like to contact you for ${data.quantity} items`;
-              window.location.href = `mailto:customademanufacturer@gmail.com?subject=Product Enquiry&body=${body}`;
-            })}
-          >
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
             <input {...register("name")} type="text" placeholder="Name" />
-            <p className="form__paragraph">{errors.Name?.message}</p>
+            <p className="form__paragraph">{errors.name?.message}</p>
             <input
               {...register("companyName")}
               type="text"
               placeholder="Company Name"
             />
-            <p className="form__paragraph">{errors.CompanyName?.message}</p>
+            <p className="form__paragraph">{errors.companyName?.message}</p>
             <input
               {...register("quantity")}
               type="text"
               placeholder="Quantity (Min 50 plus)"
             />
-            <p className="form__paragraph">{errors.Quantity?.message}</p>
+            <p className="form__paragraph">{errors.quantity?.message}</p>
             <input
               {...register("contactNumber")}
               type="text"
               placeholder="Contact Number"
             />
-            <p className="form__paragraph">{errors.ContactNumber?.message}</p>
+            <p className="form__paragraph">{errors.contactNumber?.message}</p>
             <input {...register("email")} type="text" placeholder="Email id" />
-            <p className="form__paragraph">{errors.Email?.message}</p>
+            <p className="form__paragraph">{errors.email?.message}</p>
             <input {...register("city")} type="text" placeholder="City" />
             <p className="form__paragraph">{errors.city?.message}</p>
             <button type="submit" id="submit" disabled={isDirty && !isValid}>
